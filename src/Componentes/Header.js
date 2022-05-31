251e7090c2a6a456e2f82ac638fadc2196773c35
aridsm <aniemorelanie@gmail.com>
2022-05-31 15:27:56 -0300

    add

--- src/Componentes/Header.js
+++ src/Componentes/Header.js
@@ -6,9 +6,26 @@ import styles from '../Styles/Header.module.css'
 const Header = () => {
 
   const { matches } = useMedia('(max-width: 1150px)')
+  const [isPageScrolled, setIsPageScrolled] = React.useState(false)
+
+  const ref = React.useRef()
+
+  React.useEffect(() => {
+    function scrollHeader() {
+      if (window.scrollY !== 0) {
+        setIsPageScrolled(true)
+      } else {
+        setIsPageScrolled(false)
+      }
+    }
+    window.addEventListener('scroll', scrollHeader)
+    return () => {
+      window.removeEventListener('scroll', scrollHeader)
+    }
+  }, [])
 
   return (
-    <header className={styles.header}>
+    <header className={`${styles.header} ${isPageScrolled ? styles.headerScrolled : ''}`} ref={ref}>
       <div className={styles.containerHeader}>
         <nav className={styles.nav}>
           {
--- src/Componentes/HeaderMobile.js
+++ src/Componentes/HeaderMobile.js
@@ -4,15 +4,35 @@ import styles from '../Styles/HeaderMobile.module.css'
 
 const HeaderMobile = () => {
   const [isMenuOpen, setIsMenuOpen] = React.useState(false)
+  const refBtnClick = React.useRef()
+  const refNavClick = React.useRef()
+
+  React.useEffect(() => {
+    function clickOutside({ target }) {
+      function containsElement(el) {
+        return el.current.contains(target)
+      }
+      if (!containsElement(refBtnClick) && (refNavClick && !containsElement(refNavClick))) {
+        setIsMenuOpen(false);
+
+        console.log('botao contem o target')
+      }
+    }
+    window.addEventListener('click', clickOutside)
+    return () => {
+      window.removeEventListener('click', clickOutside)
+    }
+  }, [])
 
   return (
     <>
-      <button className={styles.btnMenu} onClick={() => setIsMenuOpen(!isMenuOpen)}>
+      <button className={`${styles.btnMenu} ${isMenuOpen ? styles.active : ''}`} onClick={() => setIsMenuOpen(!isMenuOpen)} ref={refBtnClick}>
+        <div></div>
         <div></div>
       </button>
       {
         isMenuOpen &&
-        <nav className={styles.navItens}>
+        <nav className={styles.navItens} ref={refNavClick}>
           <a href='/'>Home</a>
           <a href='/'>Aulas</a>
           <a href='/'>Instrutores</a>
